20cfb3b38def519135455558ec8ac4faa3d1080a
Rafael Ramos <arkanjo.ms@gmail.com>
2019-05-26 22:06:21 -0300

    first commit

---
+++ microportal.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+STACK_NAME="microportal"
+case "$1" in
+    start)
+        echo "Starting ${STACK_NAME} stack"
+        docker-compose -p ${STACK_NAME} up -d --remove-orphans
+        ;;
+    stop)
+        echo "Stopping ${STACK_NAME} stack"
+        docker-compose -p ${STACK_NAME} stop
+        ;;
+    restart)
+        echo "Restarting ${STACK_NAME} stack"
+        docker-compose -p ${STACK_NAME} restart
+        ;;
+    clean)
+        echo "Cleaning ${STACK_NAME} stack"
+        docker-compose -p ${STACK_NAME} down -v
+        ;;
+    *)
+        echo "Accepted commands: start, stop, restart or clean"
+        ;;
+esac
---
+++ postgres/init-create-db.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER keycloak WITH ENCRYPTED PASSWORD 'keycloak';
+    CREATE DATABASE keycloak;
+    GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
+
+    CREATE USER microportalsec WITH ENCRYPTED PASSWORD 'microportalsec';
+    CREATE DATABASE microportalsec;
+    GRANT ALL PRIVILEGES ON DATABASE microportalsec TO microportalsec;
+EOSQL
